f070ac5591d3f28b84f6a8ffc09118899c25a27e
DFastje <dfastje@gmail.com>
2020-10-26 10:11:43 -0400

    Initial commit of empty project

---
+++ src/main/java/me/dfastje/emailcounter/EmailCounterApplication.java
@@ -0,0 +1,13 @@
+package me.dfastje.emailcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dfastje/emailcounter/EmailCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dfastje.emailcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
